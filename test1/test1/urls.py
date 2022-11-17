20eea38f2b0609c4504dc5e6c6ecb01478475313
zzy <1532488367@qq.com>
2022-11-18 05:21:57 +0800

    web两个文件修改提交

--- test1/test1/urls.py
+++ test1/test1/urls.py
@@ -19,3 +19,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+"""项目url
+通过urlpatterns
+列表中内容"""
--- test1/test1/wsgi.py
+++ test1/test1/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test1.settings')
 
 application = get_wsgi_application()
+"""web服务器指定web应用入口函数"""
